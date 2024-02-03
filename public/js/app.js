650c6c6a8f1dce261acc1ecd44b51183f859bc3d
ishmael <ishmaelkosh.com>
2024-02-03 05:04:35 -0600

    Remove unnecessary console.log commit

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-//messageOne.textContent = 'From JavaScript'
+//messageOne.textContent = 'From JavaScript'.
 messageTwo.textContent = ''
 
 weatherForm.addEventListener('submit', (e) => {
